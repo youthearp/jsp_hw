2749d9ef56f10219c2d248e38e41fa285fad8410
pc <pc@DESKTOP-J1LP1SO>
2020-05-06 12:20:47 +0900

    20qns

---
+++ src/jsp_hw/jdbc1/BookDAO.java
@@ -0,0 +1,32 @@
+package jsp_hw.jdbc1;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import jsp_hw.DB;
+
+public class BookDAO {
+    public static List<Book> findAll() throws Exception {
+        String sql = "SELECT b.*, c.categoryId " +
+                     "FROM book b LEFT JOIN category c ON c.categoryId = c.id";
+        try (Connection connection = DB.getConnection("book");
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+            ArrayList<Book> list = new ArrayList<Book>();
+            while (resultSet.next()) {
+                Book book = new Book();
+                book.setId(resultSet.getInt("id"));
+                book.setTitle(resultSet.getString("title"));
+                book.setAuthor(resultSet.getString("author"));
+                book.setCategoryId(resultSet.getInt("categoryId"));
+                book.setPrice(resultSet.getInt("price"));
+                book.setPublisher(resultSet.getString("publisher"));
+                list.add(book);
+            }
+            return list;
+        }
+    }
+}
